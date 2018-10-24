666a7dbe0a84ab518de089638129c90865b28609
Igor Bashmashnikov <igor731996@gmail.com>
2018-10-24 11:45:14 +0300

    final

--- src/GoldbachsConjecture.java
+++ src/GoldbachsConjecture.java
@@ -2,14 +2,22 @@ import java.math.BigInteger;
 
 public class GoldbachsConjecture {
 	public static void main(String args[]) {
-		EvenNumbersIterator evens = new EvenNumbersIterator(BigInteger.valueOf(10), BigInteger.valueOf(2));
-		for(int i = 0 ; i < 10 ; i++) {
-			System.out.println(evens.next());
-		}
+		Long startTime = System.nanoTime();
 		
-		PrimeNumbersIterator primes = new PrimeNumbersIterator();
-		for(int i = 0 ; i < 10 ; i++) {
-			System.out.println(primes.next());
+		EvenNumbersIterator evens = new EvenNumbersIterator(BigInteger.valueOf(10), BigInteger.valueOf(2));
+		
+		for(BigInteger n: evens) {
+			for(PrimeNumbersIterator primes = new PrimeNumbersIterator() ;  ;) {
+				BigInteger c = primes.next();
+				BigInteger a = n.subtract(primes.next());
+				if(a.isProbablePrime(100)) {
+					System.out.println(a.intValue() + " + " + c.intValue() +  " = " + n.intValue() + " Took " + (System.nanoTime() - startTime) + " nano seconds");
+					System.out.println("Took " + (System.nanoTime() - startTime) + " seconds");
+					break;
+				};
+				if(c.compareTo(n.divide(BigInteger.valueOf(2))) > 0 ) break;
+			}
+			if(n.compareTo(BigInteger.valueOf(1000)) == 0) break;
 		}
 	}
 }
